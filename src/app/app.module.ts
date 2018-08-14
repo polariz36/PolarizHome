564f11c41d61cef594859d31d8b862cbf0ba3949
polariz36 <polariz036@gmail.com>
2018-08-14 15:42:18 -0500

    Add animations module, add some more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PhNavComponent } from './ph-nav/ph-nav.component';
+import { PhDashboardComponent } from './ph-dashboard/ph-dashboard.component';
+
+import { routes } from './app.routes';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhNavComponent,
+    PhDashboardComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
