b1e93e522ee6a110eaa1bf5642d9bad72c498384
polariz36 <polariz036@gmail.com>
2018-08-16 15:43:28 -0500

    Add forms modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PhNavComponent } from './ph-nav/ph-nav.component';
@@ -10,19 +11,23 @@ import { PhDashboardComponent } from './ph-dashboard/ph-dashboard.component';
 
 import { routes } from './app.routes';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PhFormComponent } from './ph-form/ph-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PhNavComponent,
     PhDashboardComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    PhFormComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(routes),
-    SharedModule
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
