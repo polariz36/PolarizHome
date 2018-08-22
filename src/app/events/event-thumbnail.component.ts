704819e544e916078db6b24c6f706f32ebb21b0f
polariz36 <polariz036@gmail.com>
2018-08-22 16:01:32 -0500

    Add EventThumbnailComponent class files

---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-event-thumbnail",
+  templateUrl: "./event-thumbnail.component.html",
+  styleUrls: ["./event-thumbnail.component.scss"]
+})
+export class EventThumbnailComponent {
+  constructor() {}
+}
